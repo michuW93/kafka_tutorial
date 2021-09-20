026cb2d86cc221e258fb9ad31b69689eefb94983
michal.walkowiak93@gmail.com <michal.walkowiak93@gmail.com>
2021-09-20 16:43:54 +0200

    java producer sending message hello_world_testing_java_producer

--- src/main/java/com/github/kafka/tutorial/ProducerDemo.java
+++ src/main/java/com/github/kafka/tutorial/ProducerDemo.java
@@ -1,7 +1,31 @@
 package com.github.kafka.tutorial;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String bootstrapServers = "127.0.0.1:9092";
+
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello_world_testing_java_producer");
+
+        //send data - asynchronous
+        producer.send(record);
+        producer.flush();
+        //producer.close();
     }
 }
